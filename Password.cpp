f5a253fc7f51e51e4b01331e0575b65dc2112117
evangoertzen <evan.goertzen@gmail.com>
2024-11-13 11:50:15 -0800

    added has_mixed_case functionality

--- Password.cpp
+++ Password.cpp
@@ -25,5 +25,17 @@ int Password::count_leading_characters(string phrase){
   letter and at least one lower-case letter
 */
 bool Password::has_mixed_case(string pass){
-  return false;
+  bool containsUpper = false;
+  bool containsLower = false;
+
+  for(auto ch : pass){
+    if (ch >= 'A' && ch <= 'Z'){
+      containsUpper = true;
+    }
+    if(ch >= 'a' && ch <= 'z'){
+      containsLower = true;
+    }
+  }
+
+  return containsLower && containsUpper;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,11 +5,11 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
+class PasswordTest : public ::testing::Test
 {
 	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
+		PasswordTest(){} //constructor runs before each test
+		virtual ~PasswordTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
@@ -19,4 +19,53 @@ TEST(PasswordTest, single_letter_password)
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, mixed_case_z)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case("Zz");
+	ASSERT_EQ(true, rslt);
+}
+
+TEST(PasswordTest, mixed_case_a)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case("Aa");
+	ASSERT_EQ(true, rslt);
+}
+
+TEST(PasswordTest, non_mixed_case_lower)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case("password");
+	ASSERT_EQ(false, rslt);
+}
+
+TEST(PasswordTest, non_mixed_case_upper)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case("PASSWORD");
+	ASSERT_EQ(false, rslt);
+}
+
+TEST(PasswordTest, mixed_case_long)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case("LEKNFdsflknLEKNFslkdfLSDNFKKLKFLKnseknflksmdflklKFNlkfNLKNFDLdksNF");
+	ASSERT_EQ(true, rslt);
+}
+
+TEST(PasswordTest, mixed_case_with_special_chars)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case(";:_*&^%$#@%^&*()");
+	ASSERT_EQ(false, rslt);
+}
+
+TEST(PasswordTest, mixed_case_with_special_chars_and_true)
+{
+	Password my_password;
+	bool rslt = my_password.has_mixed_case(";:_*&^%$#@%^&*()Aa");
+	ASSERT_EQ(true, rslt);
 }
